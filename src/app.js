a2dd767a862d134d08ca95ef0a6a71befec2b04e
ayush <ayushjhanwar12@gmail.com>
2024-12-17 12:48:36 +0530

    add: cheat meal

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const connectDB = require('./config/db');
 require('dotenv').config();
+const seedCheatMeals = require("./utils/seedCheatMeals");
+const cors = require('cors');
 
 const app = express();
 
-connectDB();
-
 
 app.use(express.json());
-app.use(require('cors')());
+app.use(cors());
 
 app.use("/api/diet-plan", require("./routes/dietPlanRoutes"));
-app.use("/api/user",require("./routes/UserRoutes"));
+app.use("/api/user", require("./routes/UserRoutes"));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const startServer = async () => {
+  await connectDB();
+  await seedCheatMeals();
+  
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
