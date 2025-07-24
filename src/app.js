ba9a274646e7a0e296e8cb32b37027b7873d7a2e
Archit-KineticScape <archit@kineticscapestudios.com>
2025-07-24 13:26:04 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use("/api/admin", adminRoutes.adminRouter);
 const startServer = async () => {
   await connectDB();
 
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 6000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
